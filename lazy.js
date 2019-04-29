0b5bf9a4d4492e4b64647d4af7c010f9832445d6
James Brooks <james.brooks@sapient.com>
2019-04-29 13:22:39 +0100

    Improve evaluation code

--- lazy.js
+++ lazy.js
@@ -3,15 +3,13 @@ export const Lazy = () => {
 
   const add = fnToAdd => functionsArray.push(fnToAdd)
 
-  const evaluate = arrayOfValues => {
-    return arrayOfValues.map(item => {
-      let result = item
-      functionsArray.forEach(fn => {
-        result = fn(result)
-      })
-      return result
-    })
-  }
+  const evaluate = array =>
+    array.map(item =>
+      functionArray.reduce(
+        (lastValue, currentFunction) => currentFunction(lastValue),
+        item
+      )
+    )
 
   return {
     add,
